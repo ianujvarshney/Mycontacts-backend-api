71cddbe982e677755864d1cf8d3e1a55e6eded1d
ianujvarshney <anujkk19111997@gmail.com>
2023-05-12 23:28:29 +0530

    Server js file add and work on routing of nodejs

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("Express Starting......");
+const express = require('express');
+const dotenv = require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+app.get('/',(req,res)=>{
+    res.json({message: "hello world"})
+})
+
+app.listen(port,()=>{
+    console.log(`Port is starting..... on ${port}`);
+}); 
