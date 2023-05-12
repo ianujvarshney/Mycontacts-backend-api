1264b0ba7986692d3deed6be49c52b2a3cb89b1f
ianujvarshney <anujkk19111997@gmail.com>
2023-05-13 01:07:02 +0530

    add routes of curd operation4

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require('express');
 const dotenv = require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-app.get('/',(req,res)=>{
-    res.json({message: "hello world"})
-})
+app.use("/",require("./Routes/ContactRoutes"));
 
 app.listen(port,()=>{
     console.log(`Port is starting..... on ${port}`);
